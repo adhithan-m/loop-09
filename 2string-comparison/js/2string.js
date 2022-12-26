642ef21e9ec63244319915b1482827a34be299c5
adhithan-m <116782079+adhithan-m@users.noreply.github.com>
2022-12-26 19:41:56 +0530

    2string-finding method.

---
+++ 2string-comparison/js/2string.js
@@ -0,0 +1,14 @@
+z=prompt("enter the first string")    //venkat
+v=prompt("enter the second string")   //adhi
+if(vovel(z)>vovel(v))
+{
+	document.write(z)
+}
+else if(vovel(z)<vovel(v)){
+	alert(v)
+}
+else{
+	alert("two string are same")
+}
+alert
+
